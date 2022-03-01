f67f8435aefe2f89e150ba64cbf215de39ee998b
Kai Kuller <kavakai@gmail.com>
2022-02-28 23:07:48 -0700

    Add images

--- data.js
+++ data.js
@@ -1,3 +1,4 @@
+import heartbreaker from './images/heartbreak.jpeg'
 
 export const ryanData = {
   about: "Ryan Adams has been praised as \"one of rock's most talented songwriters” (SPIN). The 7-time Grammy nominated artist has 16 studio albums to his name, accumulating in millions of sales. He has produced albums for esteemed musicians including Willie Nelson, Jesse Malin, Jenny Lewis and has collaborated with some of music’s most influential artists such as Weezer, Fall Out Boy, America and many more. Ryan’s profound contributions to music have taken many different forms. From his songwriting, to his producing, to fronting the rock/alternative country band Whiskeytown, and forming, “Ryan Adams & The Cardinals,” his ability to connect with audiences across a variety of different mediums is undeniable. Adams’ musical creativity was recognized early on and led to his NME Best Solo Artist win in both 2003 and 2004. In addition to his self-titled, double-Grammy-nominated 2014 album, Ryan’s catalogue includes Heartbreaker, Ashes & Fire, Easy Tiger, Cold Roses, Jacksonville City Nights, Love Is Hell, his 2001 major label debut Gold, and more. Ryan’s artistry is not limited to his songwriting capabilities. In 2009, he published Infinity Blues, a book of free verse poetry about his journey to self-realization, and Hello Sunshine, a collection of poems and short stories. Ryan’s passion for writing continues, and he is currently working on his first novel. His music has spanned multiple musical genres, and has been incorporated in film and television specials such as Judd Apatow’s top grossing production Bridesmaids, This is 40, popular HBO TV series’ True Blood and Felicity, and his song, ‘Stay Alive’ appeared in The Secret Life of Walter Mitty, starring Ben Stiller. In November 2014, Ryan performed a two-night engagement at New York’s famed Carnegie Hall, which turned into a critically acclaimed live album on PAX-AM records, a label specifically for his music releases. Ryan is widely known for his powerful shows performed all over the world and regarded for his prolific songwriting. He has been praised by many music icons including Noel Gallagher, Taylor Swift and the legend Elton John, who described him as the “fabulous one.” Adams currently resides in Los Angeles, California with his five cats, typewriter, and coffee machine.",
@@ -6,7 +7,7 @@ export const ryanData = {
       id: 1,
       releaseDate: "09/05/2000",
       title: "Heartbreaker",
-      image:"",
+      image: heartbreaker,
       description: "Heartbreaker is the debut studio album by Ryan Adams, released on September 5, 2000. The album was recorded over fourteen days at Woodland Studios in Nashville, Tennessee. It was nominated for the 2001 Shortlist Music Prize. The album is said to be inspired by Adams' break-up with music industry publicist Amy Lombardi. According to Adams, the album's title originates from a poster of Mariah Carey: \"My manager called and said, 'You have 15 seconds to name this record, ' My eyes focused on this poster of Mariah wearing a T-shirt that said HEARTBREAKER. I just shouted, 'Heartbreaker!’\"'",
       tracks: [
         {
@@ -76,7 +77,7 @@ export const ryanData = {
       id: 2,
       releaseDate: "09/25/2001",
       title: "Gold",
-      image:"",
+      image: "https://upload.wikimedia.org/wikipedia/en/b/bc/Ryan_Adams_Gold.jpg",
       description: "Gold is the second studio album by Ryan Adams, released September 25, 2001. The album remains Adams' best-selling album, certifying Gold in the UK and going on to sell 364,000 copies in the US and 812,000 worldwide. Adams noted that \"with Gold, I was trying to prove something to myself. I wanted to invent a modern classic.\" The album includes \"When the Stars Go Blue\", which has been covered by artists such as The Corrs, Bono, Tyler Hilton, Bethany Joy Galeotti, and Tim McGraw.",
       track: [
         {
@@ -653,8 +654,8 @@ export const ryanData = {
       runtime: "40:22"
     },
     { id: 11,
-      releaseDate: "Orion",
-      title: "05/18/2010",
+      releaseDate: "05/18/2010",
+      title: "Orion",
       image:"",
       description: "Orion is the 11th studio album by Ryan Adams, released on May 18th, 2010. Described as Adams' first \"fully-realized sci-fi metal concept album,\" Orion was released on a limited vinyl run, and could only be purchased from the PAX AM online store. There are no plans for a wide release. Orion was recorded in 2006, and is Adams' first full-length album release since splitting from The Cardinals in March of 2009.",
       tracks: [
