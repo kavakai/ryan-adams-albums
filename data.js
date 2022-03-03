a7d166a006e39400003cc853d49b520d3bbead6d
Kai Kuller <kavakai@gmail.com>
2022-03-03 09:41:50 -0700

    Fix track titles on gold album

--- data.js
+++ data.js
@@ -100,64 +100,68 @@ const ryanData = {
       description: "Gold is the second studio album by Ryan Adams, released September 25, 2001. The album remains Adams' best-selling album, certifying Gold in the UK and going on to sell 364,000 copies in the US and 812,000 worldwide. Adams noted that \"with Gold, I was trying to prove something to myself. I wanted to invent a modern classic.\" The album includes \"When the Stars Go Blue\", which has been covered by artists such as The Corrs, Bono, Tyler Hilton, Bethany Joy Galeotti, and Tim McGraw.",
       tracks: [
         {
-          title: "(Argument with David Rawlings Concerning Morrissey)\" (An argument regarding the Morrissey track \"Suedehead\".)",
-          length: "0:37"
-        },
-        {
-          title: "To Be Young (Is to Be Sad, Is to Be High)",
-          length: "3:04"
-        },
-        {
-          title: "My Winding Wheel",
-          length: "3:13"
-        },
-        {
-          title: "Amy",
+          title: "New York, New York",
           length: "3:46"
         },
         {
-          title: "Oh My Sweet Carolina",
-          length: "4:57"
+          title: "Firecracker",
+          length: "2:51"
         },
         {
-          title: "Bartering Lines",
-          length: "3:59"
+          title: "Answering Bell",
+          length: "3:05"
         },
         {
-          title: "Call Me On Your Way Back Home",
-          length: "3:09"
+          title: "La Cienega Just Smiled",
+          length: "5:03"
         },
         {
-          title: "Damn, Sam (I Love a Woman That Rains)",
-          length: "2:08"
-        },
-        {
-          title: "Come Pick Me Up",
-          length: "5:18"
-        },
-        {
-          title: "To Be the One",
-          length: "3:01"
-        },
-        {
-          title: "Why Do They Leave?",
+          title: "The Rescue Blues",
           length: "3:38"
         },
         {
-          title: "Shakedown on 9th Street",
-          length: "2:53"
+          title: "Somehow, Someday",
+          length: "4:24"
         },
         {
-          title: "Don't Ask for the Water",
-          length: "2:56"
+          title: "When the Stars Go Blue",
+          length: "3:31"
         },
         {
-          title: "In My Time of Need",
-          length: "5:39"
+          title: "Nobody Girl",
+          length: "9:40"
         },
         {
-          title: "Sweet Lil Gal (23rd/1st)",
-          length: "3:39"
+          title: "Sylvia Plath",
+          length: "4:10"
+        },
+        {
+          title: "Enemy Fire",
+          length: "4:09"
+        },
+        {
+          title: "Gonna Make You Love Me",
+          length: "2:36"
+        },
+        {
+          title: "Wild Flowers",
+          length: "4:59"
+        },
+        {
+          title: "Harder Now That It's Over",
+          length: "4:32"
+        },
+        {
+          title: "Touch, Feel & Lose",
+          length: "4:15"
+        },
+        {
+          title: "Tina Toledo's Street Walkin' Blues",
+          length: "6:10"
+        },
+        {
+          title: "Goodnight, Hollywood Blvd.",
+          length: "3:25"
         },
       ],
       runtime: "70:26"
